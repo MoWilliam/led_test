bf57f1fa9c14689851393630d2e884a23fe48877
MoWilliam <124038064+MoWilliam@users.noreply.github.com>
2024-04-24 11:09:54 +0800

    Add files via upload

---
+++ applications/application.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-01-11     Administrator       the first version
+ */
+#ifndef APPLICATIONS_APPLICATION_H_
+#define APPLICATIONS_APPLICATION_H_
+#include "stm32f1xx_hal.h"
+#include "ut/inc/ut_type.h"
+#include "ut/inc/ut_message.h"
+#include "ut/inc/ut_mq.h"
+#include "ut/inc/ut_thread.h"
+#include "manage/inc/m_device.h"
+
+
+#define DeBug 0   //调试窗口
+
+
+static struct rt_mutex mutex;
+
+
+
+#endif /* APPLICATIONS_APPLICATION_H_ */
